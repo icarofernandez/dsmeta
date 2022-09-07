7a2f10757676ccb1a834dd2055c92875e3224e50
Icaro Fernandez <icarocanalgomes@gmail.com>
2022-09-06 21:32:19 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
