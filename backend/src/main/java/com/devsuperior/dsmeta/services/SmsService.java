307b993c57c3bdf570375d5017d4c3187f91c910
Icaro Fernandez <icarocanalgomes@gmail.com>
2022-09-07 23:06:00 -0300

    Heroku4

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -24,17 +24,18 @@ public class SmsService {
 
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
-	
+
 	@Autowired
 	private SaleRepository saleRepository;
 
 	public void sendSms(Long saleId) {
-		
+
 		Sale sale = saleRepository.findById(saleId).get();
-		
+
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		
-		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
+
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
